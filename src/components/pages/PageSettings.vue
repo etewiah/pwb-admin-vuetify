c4e6b1ed1b166d45a76cc11c945456cb3fcc659a
Ed Tewiah <etewiah@hotmail.cim>
2018-02-20 16:28:00 +0100

    Use FormSubmitter with PageSettings component

--- src/components/pages/PageSettings.vue
+++ src/components/pages/PageSettings.vue
@@ -1,31 +1,47 @@
 <template>
   <v-layout row>
     <v-flex xs6>
-      <form @submit.prevent="onUpdatePageSettings">
-        <v-layout v-for="(field, index) in titleFields" row>
-          <v-flex xs12 sm12 offset-sm0>
-            <v-text-field name="title" :label="'Title ' + $t(field.labelTextTKey)" id="title" v-model="currentPage[field.fieldName]"></v-text-field>
-          </v-flex>
-        </v-layout>
-        <v-layout row>
-          <v-flex xs12 sm12 offset-sm0>
-            <v-text-field name="title" label="Title" id="title" v-model="currentPage['page_title_es']" required></v-text-field>
-          </v-flex>
-        </v-layout>
-        <v-layout row>
-          <v-flex xs12 sm6 offset-sm0>
-            <v-btn class="primary" :disabled="!formIsValid" type="submit">Update</v-btn>
-          </v-flex>
-        </v-layout>
-      </form>
+      <v-card class="mt-3">
+        <v-card-title primary-title>
+          <div>
+            <h4 class="headline mb-0">{{ $t('en') }}</h4>
+          </div>
+        </v-card-title>
+        <v-card-text>
+          <form @submit.prevent="onUpdatePageSettings">
+            <v-layout row>
+              <FormSubmitter :hasPendingChanges="hasPendingChanges"></FormSubmitter>
+            </v-layout>
+            <v-layout v-for="(field, index) in pageTitleFields" row>
+              <v-flex xs12 sm12 offset-sm0>
+                <v-text-field name="title" v-on:keyup="onTitleChange(field.fieldName, field.fieldValue)" :label="'Title ' + $t(field.labelTextTKey)" v-model="field.fieldValue"></v-text-field>
+              </v-flex>
+            </v-layout>
+            <v-layout row>
+              <FormSubmitter :hasPendingChanges="hasPendingChanges"></FormSubmitter>
+            </v-layout>
+          </form>
+        </v-card-text>
+        <v-card-actions>
+          <!-- <v-btn flat color="orange">Share</v-btn> -->
+        </v-card-actions>
+      </v-card>
     </v-flex>
   </v-layout>
 </template>
 <script>
+import FormSubmitter from '@/components/form-fields/FormSubmitter'
+import _ from 'lodash'
 export default {
+  components: {
+    FormSubmitter
+  },
   props: ["currentPage"],
   data() {
     return {
+      pendingChanges: {},
+      // originalValues: {},
+      hasPendingChanges: false,
       titleFields: [{
         labelTextTKey: "webContentLabels.suffixEn",
         fieldType: "simpleInput",
@@ -47,33 +63,58 @@ export default {
     }
   },
   computed: {
-    // currentPage() {
-    //   return this.$store.state.currentProperty
+    // hasPendingChanges() {
+    //   let pendingChanges = this.pendingChanges
+    //   let pcl = Object.keys(pendingChanges).length
+    //   return pcl > 0
     // },
-    formIsValid() {
-      return this.title !== '' &&
-        this.location !== '' &&
-        this.imageUrl !== '' &&
-        this.description !== ''
+    supportedLocales() {
+      return this.$store.state.supportedLocales
     },
+    pageTitleFields() {
+      let pageTitleFields = []
+      let that = this
+      this.supportedLocales.forEach(function(locale) {
+        let field = {}
+        field.labelTextTKey = "webContentLabels.suffix" + locale.substring(0, 1).toUpperCase() + locale.substring(1, 2)
+        field.fieldName = "page_title_" + locale.substring(0, 2)
+        field.fieldValue = _.cloneDeep(that.currentPage[field.fieldName])
+        pageTitleFields.push(field)
+        // if (that.currentPage[field.fieldName]) {
+        //   that.originalValues[field.fieldName] = _.cloneDeep(that.currentPage[field.fieldName]) 
+        //   // JSON.parse(JSON.stringify(that.currentPage[field.fieldName]))
+        // }
+      })
+      return pageTitleFields
+    },
+    // formIsValid() {
+    //   return this.title !== '' &&
+    //     this.location !== '' &&
+    //     this.imageUrl !== '' &&
+    //     this.description !== ''
+    // },
   },
-  mounted: function() {
+  mounted() {
     // this.$store.dispatch('loadProperty', this.$route.params["id"])
   },
   methods: {
+    onTitleChange(fieldName, newValue) {
+      if (this.currentPage[fieldName] !== newValue) {
+        this.pendingChanges[fieldName] = newValue
+      } else {
+        delete this.pendingChanges[fieldName]
+      }
+      this.hasPendingChanges = Object.keys(this.pendingChanges).length > 0
+    },
     onUpdatePageSettings() {
-      // if (!this.formIsValid) {
-      //   return
-      // }
-      // const meetupData = {
-      //   title: this.title,
-      //   location: this.location,
-      //   imageUrl: this.imageUrl,
-      //   description: this.description,
-      //   date: this.submittableDateTime
-      // }
-      // debugger
+      var that = this
+      Object.keys(this.pendingChanges).forEach(function(pendingChangeKey) {
+        that.currentPage[pendingChangeKey] = that.pendingChanges[pendingChangeKey]
+      })
       this.$store.dispatch('updatePage')
+
+      this.pendingChanges = {}
+      this.hasPendingChanges = false
     }
 
   }
